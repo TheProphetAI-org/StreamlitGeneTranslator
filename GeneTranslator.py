20f24f93aa3611660c2007cb6ab4ff07a2237fd3
GiacominiTPAI <82030620+GiacominiTPAI@users.noreply.github.com>
2023-03-03 14:31:30 +0100

    Update GeneTranslator.py

--- GeneTranslator.py
+++ GeneTranslator.py
@@ -4,8 +4,8 @@ import numpy as np
 import psycopg2
 import pyperclip
 
+
 #functions
-@st.cache_resource
 def connect():
     connection = psycopg2.connect(user="gene_db_light_user",
                                 password="l9KqjzIpUFh0VTfW9N6871fBxa7B2GmX",
@@ -18,7 +18,7 @@ def connect():
 def build_query(gene,source):
     #postgreSQL_select_Query = 'SELECT "GeneSym", "UniprotID" FROM public."GeneTab" WHERE "UniprotID"=\'{}\''
     if source == 'All':
-        query ='SELECT "genesymbol" FROM public."GeneTab_Light" WHERE \'{}\' in ("genesymbol","uniprot","biogrid","chembl","ensable_protein_id","ensemblid","hgnc","name","ncbi","alias") LIMIT 1'.format(gene)
+        query ='SELECT "genesymbol" FROM public."GeneTab_Light" WHERE \'{}\' in ("genesymbol","uniprot","biogrid","chembl","string","ensemblid","hgnc","name","ncbi","alias") LIMIT 1'.format(gene)
     else:
         query ='SELECT "genesymbol" FROM public."GeneTab_Light" WHERE "{}" = \'{}\''.format(source,gene)
         print(query)
@@ -28,7 +28,7 @@ def build_query(gene,source):
 def get_cols(_cursor):
     cursor.execute('SELECT column_name FROM information_schema.columns WHERE table_name = \'GeneTab_Light\'')
     cols=pd.DataFrame(cursor.fetchall())
-    options=pd.concat([pd.DataFrame(['All']).append(cols)])
+    options=pd.concat([pd.DataFrame(['Select the Source','All']).append(cols)])
     return options
 
 def preprocess_text_input(text):
@@ -62,17 +62,19 @@ source=st.selectbox("Chose your nomenclature, if 'All' selected the system will 
 
 
 #Buttons
+output_type = st.radio("Select the output type",["GeneSymbol Only","Original Nomenclature and GeneSymbol"])
 col1,col2= st.columns(2)
+placeholder_bar=st.empty()
 bt=col1.button("Translate", key=1)
 placeholder= col2.empty()
 df=preprocess_text_input(text)
-
+col3,col4=st.columns(2)
 
 if bt:
     genetext=""
-    gene_org=""
+    gene_org=pd.DataFrame()    
     placeholder.write(f"⏳")
-    my_bar = st.progress(0)
+    my_bar = placeholder_bar.progress(0)
     for index, row in df.iterrows():
         my_bar.progress(index/len(df)+1/len(df))
         try:
@@ -80,13 +82,18 @@ if bt:
             selection = cursor.fetchall()
             result = pd.DataFrame(selection)
         except:
-            st.write("Error: Please reload the page")
-        col1.text(row[0])
-        gene_org=gene_org+"\n"+ row[0]
+            st.write("Error: Please reload the page2")
+        col3.text(row[0])
         try:
-            col2.text(result[0].values[0])
+            col4.text(result[0].values[0])
             genetext=genetext+"\n"+ result[0].values[0]
+            gene_org=gene_org.append([[row[0],result[0].values[0]]])
         except:
-            col2.text('na')
-    placeholder.download_button(label="Download data as txt file", data=genetext,file_name='GeneSymbol.txt',mime='text/csv',)
+            col4.text('na')
+            gene_org=gene_org.append([[row[0],'na']])
+    
+    if output_type=="GeneSymbol Only":
+        placeholder.download_button(label="Download data as txt file", data=genetext,file_name='GeneSymbol.txt',mime='text/csv',)
+    else:
+         placeholder.download_button(label="Download data as csv file", data=gene_org.to_csv(),file_name='OriginalandGeneSymbol.csv',mime='text/csv',)
 
